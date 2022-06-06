98771101a3ad592fc231379d677c07a8aa4c680c
Mark Zacharias <mark@punchcard.io>
2022-06-05 21:06:39 -0600

    add notification classes as per tutorial

---
+++ notifications/NotficationConfig.tsx
@@ -0,0 +1,7 @@
+const notificationConfig =  {
+    appName: 'Notification Sample',
+    env: 'production',
+    apiUrl: 'https://<azure-push-notifications-api-url>/api/',
+    apiKey: '<api-auth-key>',
+};
+export default notificationConfig;
